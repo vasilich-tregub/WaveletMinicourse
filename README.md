c6f4398dea27a4cacca1d2b2bd9c27acf7ac30f4
V.V.T. <vasilich.tregub@gmail.com>
2026-04-21 17:22:53 +0700

    int-to-int lossless DWT

--- README.md
+++ README.md
@@ -2,3 +2,19 @@
 
 A minicourse to help CPU and FPGA programmers understand, use and implement DWT for JPEG XS codecs.
 
+* In the executable 'haar-biorthogonal', the Haar wavelet transform operates over a ring of integers: 
+see comments in lines 29-30 and 61-62 of 'haar-biorthogonal.cpp' for explanation.
+
+* A pair of direct and inverse Le Gall 5/3 transforms as implemented in the 'legall53' folder (and 
+similar ones throughout this project) guarantees a lossless coding over a ring of integers. The 
+trick used there is summarized in 
+[THE LIFTING SCHEME: A CONSTRUCTION OF SECOND GENERATION WAVELETS by Wim Sweldens](https://cm-bell-labs.github.io/who/wim/papers/lift2.pdf) 
+on page 35:
+
+`14.11. Integer to integer wavelet transforms. In [16] lifting is used to build reversible
+wavelets which map integers to integers for applications to lossless image coding. The idea is
+to introduce a non-linear round-off in each lifting step. This way the result is guaranteed to 
+be integer while the lifting assure that the transform is invertible. The exact same idea works 
+in the second generation setting and using this in second generation compression applications 
+is another line of future research.`
+
---
+++ haar/haar-biorthogonal.cpp
@@ -0,0 +1,73 @@
+﻿// HAAR-interleaved.cpp : Defines the entry point for the application.
+//
+
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	std::vector<int> im{ 7, 10, 8, 6, 4, 1, 3, 7 };
+	std::cout << "Source:" << "\n";
+	for (int i = 0; i < im.size(); ++i)
+	{
+		std::cout << '#' << i << ":" << im[i] << "; ";
+	}
+	std::cout << "\n";
+	size_t len = im.size();
+	std::vector<std::vector<int>> decomp;
+	std::cout << "Direct biorthogonal Haar transform:" << "\n";
+	std::vector<int> a(im);
+	int level = 1;
+	while (len > 1)
+	{
+		std::vector<int> d(len);
+		len = len / 2;
+		for (int i = 0; i < len; ++i)
+		{
+			int even = a[2 * i];
+			int odd = a[2 * i + 1];
+			d[i] = (even + odd);			// Note that values (even + odd) and (even - odd) have identical parity
+			d[i + len] = (even - odd);		// d[i] (approx) and d[i + len] (coeff) are either both even or both odd
+		}
+		decomp.emplace_back(d);
+		std::cout << "level " << level << ":\n";
+		for (int i = 0; i < 2 * len; ++i)
+		{
+			a[i] = d[i];
+			std::cout << '#' << i << ":" << d[i] << "; ";
+		}
+		std::cout << "\n";
+		++level;
+	}
+	std::cout << "\n";
+	std::cout << "Decomposition:" << "\n";
+	for (int i = 0; i < im.size(); ++i)
+	{
+		std::cout << '#' << i << ":" << a[i] << "; ";
+	}
+	std::cout << "\n" << "\n";
+	std::cout << "Inverse biorthogonal Haar transform:" << "\n";
+	std::vector<int> recovered(im.size());
+	--level;
+	while (level > 0)
+	{
+		len = decomp[--level].size();
+		std::vector<int> recovered_coeffs(len);
+		len = len / 2;
+		for (int i = 0; i < len; ++i)
+		{
+			int approx = a[i];
+			int coeff = a[i + len];
+			recovered_coeffs[2 * i] = (approx + coeff) / 2;			// see lines 29, 30. 'approx' and 'coeff' values
+			recovered_coeffs[2 * i + 1] = (approx - coeff) / 2;		// of the same decomp level have identical parity.
+		}
+		std::cout << "level " << level << "(recovered values):\n";
+		for (int i = 0; i < 2 * len; ++i)
+		{
+			a[i] = recovered_coeffs[i];
+			std::cout << '#' << i << ":" << recovered_coeffs[i] << "; ";
+		}
+		std::cout << "\n";
+	}
+	return 0;
+}
